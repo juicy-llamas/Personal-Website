2cea5a58af4dd0a9181600e5f5cfea5b0fec2f0a
Daniel Moylan <demoylan@gmail.com>
2021-09-13 12:45:13 -0400

    new folder / overhaul

---
+++ Production/FINISHED/js/circle-run.js
@@ -0,0 +1,24 @@
+ 
+/*
+ * Circle Controller
+ *
+ * (c) Daniel Moylan 2021
+ */
+
+"use strict";
+
+const circle = ( function () {
+
+	// Defines number of circles.
+	const NUM_OF_CIRCLES = 5;
+	// Defines spin rates.
+	const RESTING_SPIN_RATE = .2;
+	const SPINNY_SPIN_RATE = 20;
+	const OVER_SPIN_RATE = 0;
+	const SPIN_TOLERANCE = 0.01;
+
+	const circles = [];
+	const circGl = circle_gl( NUM_OF_CIRCLES );
+
+
+} )();
