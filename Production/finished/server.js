d2821d07f883b57a664e2edcd43acc1b319e4124
juicy-llamas <demoylan@gmail.com>
2023-01-04 20:38:57 -0500

    FINISHED AAAAAAAA

--- Production/finished/server.js
+++ Production/finished/server.js
@@ -5,6 +5,13 @@ const Port = process.env.port || 8000;
 
 app.use( '/', express.static( 'static' ) );
 
+// can remove if not windows
+{
+	const os = require( 'os' );
+	if ( os.version().includes( 'dows' ) )
+		app.use( '/Assets', express.static( 'Assets' ) );
+}
+
 app.listen( Port, ( e ) => {
 	if ( e !== undefined )
 		console.err( `An error occurred: ${e}` );
